0e61258567d495957892eded4d05107073d74384
H-Stranger <3547845923@qq.com>
2023-08-02 22:42:36 +0800

    C learning

---
+++ 内存/test1.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+	////内置类型
+	////整型家族
+	//char
+	//short(short int)
+	//int 
+	//long(long int)
+	//long long
+	////浮点型家族
+	//float
+	//double
+	//long double
+
+
+
+//int test1()
+//{
+//	printf("hello world\n");
+//	return 0;
+//}
+//int test2(void)
+//{
+//	printf("hello world\n");
+//	return 0;
+//}
+//int main()
+//{
+//	//未定义参数的函数实际应用时添加一些参数并不会报错
+//	//void则强调此处不可添加参数
+//	test1(10);
+//	test2(10);//err
+//	return 0;
+// }
+
+int main()
+{
+	unsigned char a = -7;
+	printf("%d\n", a);
+
+	return 0;
+}
