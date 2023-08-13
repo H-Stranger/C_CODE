a03b540373aeb1516163eb7c955e147324c14f96
H-Stranger <3547845923@qq.com>
2023-08-13 15:04:13 +0800

    C Learning

--- 指针 进阶/test9.c
+++ 指针 进阶/test9.c
@@ -1,26 +1,109 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+//
+// 面试题
+// 
 //int main()
 //{
 //	int arr[3][4] = { 0 };
 //	
 //	printf("%d\n", sizeof(arr + 1));
+//	//第二行地址
+//	
 //	return 0;
 //}
 
-struct test 
-{
-	int Num;
-	char* pcName;
-	short sDate;
-	char cha[2];
-	short sBa[4];
-} *p;
-int main()
-{
-	p = (struct test*)0x100000;
-	printf("%p\n", p + 0x1);
-	printf("%p\n", (unsigned long)p + 0x1);
-	printf("%p\n", (unsigned long)p + 1);
-	return 0;
-}
+
+//2.
+//int main()
+//{
+//	int a[5] = { 1,2,3,4,5 };
+//	int* ptr = (int*)(&a + 1);
+//	printf("%d,%d\n", *(a + 1), *(ptr - 1));
+//
+//	return 0;
+//}
+
+//3.
+//struct test 
+//{
+//	int Num;
+//	char* pcName;
+//	short sDate;
+//	char cha[2];
+//	short sBa[4];
+//} *p;
+//int main()
+//{
+//	p = (struct test*)0x100000;
+//	printf("%p\n", p + 0x1);
+//	printf("%p\n", (unsigned long)p + 0x1);
+//	printf("%p\n", (unsigned long)p + 1);
+//	return 0;
+//}
+
+
+//4.
+//int main()
+//{
+//	int a[3][2] = { (0,1),(2,3),(4,5) };
+//	int* p;
+//	p = a[0];
+//	printf("%d\n", p[0]);
+//
+//	return 0;
+//}
+
+
+//5.
+//int main()
+//{
+//	int a[5][5];
+//	int(*p)[4];
+//	p = a;
+//	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+//
+//	return 0;
+//}
+
+//6.
+//int main()
+//{
+//	int aa[2][5] = { 1,2,3,4,5,6,7,8,9,10 };
+//
+//	int* ptr1 = (int*)(&aa + 1);
+//	int* ptr2 = (int*)(*(aa + 1));
+//
+//	printf("%d,%d\n", *(ptr1 - 1), *(ptr2 - 1));
+//
+//	return 0;
+//}
+
+
+//7.
+//int main()
+//{
+//	char* a[] = { "work","at","alibaba" };
+//	char** pa = a;
+//
+//	pa++;
+//	printf("%s\n", *pa);
+//
+//	return 0;
+//}
+
+
+
+//8.
+//int main()
+//{
+//	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+//	char** cp[] = { c + 3,c + 2,c + 1,c };
+//	char*** cpp = cp;
+//	printf("%s\n", **++cpp);
+//	printf("%s\n", *-- * ++cpp + 3);
+//	printf("%s\n", *cpp[-2] + 3);
+//	printf("%s\n", cpp[-1][-1] + 1);
+//
+//	return 0;
+//}
